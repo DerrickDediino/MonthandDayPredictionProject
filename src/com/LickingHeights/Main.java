8b1a551daac7b00bbd98e314538d19282321ebb1
Derrick <Derrick.Dedino.2022@student.lhschools.org>
2019-04-29 14:16:07 -0400

    Poem Reading method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,17 +8,18 @@ public class Main {
 	Scanner keyboard = new Scanner(System.in);
 	boolean loop = true;
 
-	System.out.println("What year were you born in?");
 
 	while (loop){
-        System.out.println("Which month were you born in (number form)?");
+        System.out.println("What year were you born in?");
         int year = keyboard.nextInt();
+        System.out.println("Which month were you born in (number form)?");
         int m = keyboard.nextInt();
         System.out.println(monthName(m) + " has " + monthDays(m,year) + " days.");
 
         System.out.println("What day of the month were you born on (number form)?");
         int q = keyboard.nextInt();
         System.out.println("You were born on a " + dayOfTheWeek(q, m, year));
+        System.out.println(poemLine(dayOfTheWeek(q,m,year)));
 
         }
     }
@@ -119,6 +120,26 @@ public class Main {
                 return "Thursday";
             case 6:
                 return "Friday";
+        }
+        return "Error";
+    }
+    public static String poemLine(String h){
+        switch (h){
+            case "Saturday":
+                return "Saturday's child works hard for a living";
+            case "Sunday":
+                return "But the child born on Sabbath Day,\n"+
+                "Is fair and wise and good in every way";
+            case "Monday":
+                return "Monday's child is fair of face";
+            case "Tuesday":
+                return "Tuesday's child is full of grace";
+            case "Wednesday":
+                return "Wednesday's child is full of woe";
+            case "Thursday":
+                return "Thursday's child has far to go";
+            case "Friday":
+                return "Friday's child is loving and giving,";
         }
         return "Error";
     }
